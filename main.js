ffddd4d161d1deee836f6d970ff194baf3b22096
fouz0062 <fouzifathii@gmail.com>
2026-01-13 21:52:04 +0100

    created a base crud template

--- main.js
+++ main.js
@@ -6,6 +6,19 @@ const port = 3000;
 app.get("/", (req, res) => {
   res.json({ message: "Hello, World!" });
 });
+//CRUD operations
+
+// Create - plural
+app.post("/movies", () => {});
+
+//read - plural
+app.get("/movies", () => {});
+
+//update - singular
+app.put("/movies/:id", () => {});
+
+//delete - singular
+app.delete("/movies/:id", () => {});
 
 app.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`);
